8529410736c13d44d105e6dc41fd55a0e6c4d035
Dominik <dominik@profi-webspace.com>
2023-01-18 11:53:28 +0100

    fixed issues with search and query string

--- src/Enums/SearchMode.php
+++ src/Enums/SearchMode.php
@@ -2,6 +2,8 @@
 
 namespace Lacodix\LaravelModelFilter\Enums;
 
+use Illuminate\Database\Eloquent\Builder;
+
 enum SearchMode
 {
     case EQUAL;
@@ -9,13 +11,23 @@ enum SearchMode
     case STARTS_WITH;
     case ENDS_WITH;
 
-    public function fromString(string $value): self
+    public static function fromString(string $value): self
     {
         return match (strtolower($value)) {
             'starts_with' => self::STARTS_WITH,
             'ends_with' => self::ENDS_WITH,
             'equal' => self::EQUAL,
             default => self::LIKE,
+        };
+    }
+
+    public function applyQuery(Builder $query, string $field, string $search): Builder
+    {
+        return match ($this) {
+            self::EQUAL => $query->orWhere($field, $search),
+            self::STARTS_WITH => $query->orWhere($field, 'LIKE', $search . '%'),
+            self::ENDS_WITH => $query->orWhere($field, 'LIKE', '%' . $search),
+            default => $query->orWhere($field, 'LIKE', '%' . $search . '%'),
         };
     }
 }
