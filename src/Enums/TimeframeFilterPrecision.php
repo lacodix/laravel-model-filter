d560d4e9927111c8ab11c8e8cd3a4a69d6f8e303
Dominik Müller <dominik@lacodix.de>
2024-03-27 10:27:30 +0100

    Reapply "feat(precisiondate): added possibility to format precision date in needed format and request precision"
    This reverts commit 0803e564bc54249520f3a0a1409fdbf6f18ec24f.

--- src/Enums/TimeframeFilterPrecision.php
+++ src/Enums/TimeframeFilterPrecision.php
@@ -2,9 +2,20 @@
 
 namespace Lacodix\LaravelModelFilter\Enums;
 
+use Carbon\Carbon;
+
 enum TimeframeFilterPrecision
 {
     case DAY;
     case MONTH;
     case YEAR;
+
+    public function getPrecisionDate(Carbon $date): string
+    {
+        return match ($this) {
+            self::DAY => $date->format('Y-m-d'),
+            self::MONTH => $date->format('Y-m'),
+            self::YEAR => $date->format('Y'),
+        };
+    }
 }
