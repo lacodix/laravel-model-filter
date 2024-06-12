07f89663991395969825f3c5a2c01b9d2d0115ab
Dominik Müller <dominik@lacodix.de>
2024-06-12 17:32:57 +0200

    fix(make): fixed make command, small improvement in search-fields for joins

--- src/Traits/IsSearchable.php
+++ src/Traits/IsSearchable.php
@@ -47,8 +47,8 @@ trait IsSearchable
     {
         return $query->where(
             fn (Builder $searchQuery) => $this->searchable($searchable)
-                ->each(static fn (SearchMode $mode, string $field)
-                    => $mode->applyQuery($searchQuery, $query->qualifyColumn($field), $search))
+                ->each(static fn (SearchMode $mode, string $field) => $mode
+                    ->applyQuery($searchQuery, $query->qualifyColumn($field), $search))
         );
     }
 }
